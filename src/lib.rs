ea8a7be5412c5ea73a71bdd6f4288e2fb78a2699
Aijaz A. Ansari <aijaz@aijaz.net>
2019-10-01 16:54:47 -0600

    Removed unused code

--- src/lib.rs
+++ src/lib.rs
@@ -3,20 +3,8 @@
 mod front_of_house {
     mod hosting {
         fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
     }
 }
-
-
 
 #[cfg(test)]
 mod tests {
